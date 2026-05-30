0b6c120793033d8de71d2196493c40cd3b0ac931
Yogesh Logashanmugam <yogesh.logshan@gmail.com>
2026-05-30 22:37:30 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   // Flush after every printf
   setbuf(stdout, NULL);
 
   // TODO: Uncomment the code below to pass the first stage
-  // printf("$ ");
+  printf("$ ");
 
   return 0;
 }
