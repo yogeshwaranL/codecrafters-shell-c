b432e75c2af1fa7db53514c8354a7883e070c90b
Yogesh Logashanmugam <yogesh.logshan@gmail.com>
2026-05-31 04:34:01 +0000

    challange 2: Handle invalid commands
    Get a command as input from user and print it as invalid.
    
    Signed-off-by: Yogesh Logashanmugam <yogesh.logshan@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
   // Flush after every printf
   setbuf(stdout, NULL);
 
+  // Buffer to hold input command
+  char cmd_in[256];
+
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+
+  // Read the user command into cmd_in, it contains newline
+  fgets(cmd_in, sizeof(cmd_in), stdin);
+
+  // Remove newline from cmd_in
+  cmd_in[strlen(cmd_in) - 1] = '\0';
+
+  printf("%s: command not found\n", cmd_in);
 
   return 0;
 }
